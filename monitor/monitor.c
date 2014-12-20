ed46d41405370f24a1646635383d13c4dc2fc1c5
Matt Stock <stock@bexkat.com>
2014-12-20 15:36:32 -0500

    more tests of array indexing and memory reads

--- monitor/monitor.c
+++ monitor/monitor.c
@@ -1,4 +1,4 @@
-int si;
+unsigned i;
 unsigned *matrixptr;
 unsigned *rgbptr;
 
@@ -6,12 +6,11 @@ void foo(void) {
   while (1) {
     rgbptr = (void *)0xff400004;
     matrixptr = (void *)0xff410000;
-    for (si=0; si < 512; si++) {
+    for (i=0; i < 512; i++) {
       if (*rgbptr < 4)
-        *matrixptr = 0x40106000;
+        matrixptr[i] = 0x40106000 + (*rgbptr << 9);
       else
-	*matrixptr = 0x00500000;
-      matrixptr++;
+	matrixptr[i] = 0x00500000 + (*rgbptr << 12);
     }
     rgbptr = (void *)0xff400000;
     *rgbptr = 0x40106000;
