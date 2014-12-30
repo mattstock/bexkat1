caee9c8bc0742e78dd19cda24b114f676ea155f0
Matt Stock <stock@bexkat.com>
2014-12-30 00:13:03 -0500

    updated test code

--- monitor/memtest.c
+++ monitor/memtest.c
@@ -3,7 +3,11 @@ unsigned int r = 0x100;
 unsigned int b2;
 
 void foo(void) {
-  b++;
-  r++;
-  b2 = r + b;
+  unsigned int x = 16;
+  unsigned int y = 2;
+
+  y++;
+  y += 22;
+  x = x + 1;
+
 }
--- monitor/monitor.c
+++ monitor/monitor.c
@@ -1,13 +1,13 @@
 unsigned i;
-unsigned *matrixptr;
+unsigned *matrixptr = (void *)0xff410000;
 unsigned *rgbptr;
+unsigned *swptr = (void *)0xff400040;
 
 void foo(void) {
   while (1) {
     rgbptr = (void *)0xff400004;
-    matrixptr = (void *)0xff410000;
     for (i=0; i < 512; i++) {
-      if (*rgbptr < 4)
+      if (*rgbptr < *swptr)
         matrixptr[i] = 0x40106000 + (*rgbptr << 9);
       else
 	matrixptr[i] = 0x00500000 + (*rgbptr << 12);
