41fa2dd53f97e1b2242642895a04512f5bfbe604
Matt Stock <stock@bexkat.com>
2015-02-11 22:36:21 -0500

    removed unused assignment types

--- fpga/registerfile.v
+++ fpga/registerfile.v
@@ -4,7 +4,7 @@ input clk;
 input rst_n;
 input [COUNTP-1:0] read1, read2, write_addr;
 input [WIDTH-1:0] write_data;
-input [3:0] write_en;
+input [1:0] write_en;
 output [WIDTH-1:0] data1, data2;
 
 parameter WIDTH=32;
@@ -34,13 +34,9 @@ begin
   for (int i=0; i < COUNT; i = i + 1)
     regfile_next[i] = regfile[i];
   if (write_en[0]) 
-    regfile_next[write_addr][7:0] = write_data[7:0];
+    regfile_next[write_addr][15:0] = write_data[15:0];
   if (write_en[1]) 
-    regfile_next[write_addr][15:8] = write_data[15:8];
-  if (write_en[2]) 
-    regfile_next[write_addr][23:16] = write_data[23:16];
-  if (write_en[3]) 
-    regfile_next[write_addr][31:24] = write_data[31:24];
+    regfile_next[write_addr][31:16] = write_data[31:16];
 end
 
 endmodule
