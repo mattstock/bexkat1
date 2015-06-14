ed815bccd912aeb827b1b3e2da8ab170e9a2a755
Matt Stock <stock@csgeeks.org>
2015-06-14 13:23:37 -0700

    use new library module

--- monitor/frametests.c
+++ monitor/frametests.c
@@ -3,23 +3,6 @@
 
 int a(int x);
 int b(int x, int y, int z);
-void bitprint(unsigned row, unsigned val);
-
-void bitprint(unsigned row, unsigned val) {
-  unsigned int i=0;
-
-  while (i < 32) {
-    matrix[row*32+i] = 0x00000000;
-    i++;
-  }
-  i = 31;
-  while (val) {
-    if (val & 0x1)
-      matrix[row*32+i] = 0x00ff0000;
-    val = val >> 1;
-    i--;
-  }
-}
 
 int a(int x) {
   unsigned int h = 32;
@@ -40,9 +23,9 @@ void main(void) {
   matrix[0] = 0x00ff0000;
   c = random(2000);
   matrix[0] = 0x0000ff00;
-  bitprint(3, c);
+  matrix_bitprint(3, c);
   matrix[0] = 0x000000ff;
-  bitprint(4, 10);
+  matrix_bitprint(4, 10);
   matrix[1] = 0x000000ff;
   matrix[2] = 0x00ff0000;
   delay(10000);
