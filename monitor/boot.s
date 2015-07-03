dd97f5d9391f82c65c32a880eadb42f559ce113d
Matt Stock <stock@csgeeks.org>
2015-07-03 11:36:41 -0700

    improved simple _start

--- monitor/boot.s
+++ monitor/boot.s
@@ -1,20 +1,16 @@
 .globl _start
 _start:
-ldiu %31, 0x4000
-ldi %13, _erodata
-ldi %14, _data
+ldiu %sp, 0x4000
+ldi %0, _erodata
+ldi %1, _data
+ldi %2, _edata
 jmpd .LC3
 .LC2:
-mov %29, %14
-ldiu %28, 4
-add %14, %29, %28
-mov %27, %13
-add %13, %27, %28
-ld.l %28, (%27)
-st.l %28, (%29)
+ld.l %3, (%0)
+addi %0, %0, 4
+st.l %3, (%1)
+addi %1, %1, 4
 .LC3:
-mov %29, %14
-ldi %28, _edata
-cmp %29, %28
+cmp %1, %2
 bltu .LC2
 jmpd main
