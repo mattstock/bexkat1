d6bb187859e83b579c92e1f6d76059dc2549da3a
Matt Stock <stock@csgeeks.org>
2015-09-05 19:30:50 -0400

    demo of font generation on matrix and LCD

--- monitor/kernel/kernel.c
+++ monitor/kernel/kernel.c
@@ -2,40 +2,62 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
-#include "serial.h"
+#include <unistd.h>
+#include "itd.h"
+#include "matrix.h"
+#include "misc.h"
 
-int x;
+extern unsigned font88[];
 
-int ordering(int a, int b, int c, int d, int e, int f, int g) {
-  int m, n, o, p;
+// x,y defines the upper left corner for now
+void drawchar(int x, int y, char c) {
+  // need 2 unsigned for the 8x8 char
+  int a, b, v, w, on;
+  unsigned bitmap;
 
-  m = 1;
-  n = 2;
-  o = 3;
-  p = 4;
-  a = 100;
-  b = 200;
-  c = 300;
-  d = 400;
-  e = 500;
-  f = 600;
-  g = 700;
-  return a;
-}
-
-int test(const char *s, ...) {
-  const char *p;
-  va_list argp;
-
-  va_start(argp, s);
-  x = va_arg(argp, int);
-  
-  va_end(argp);
-  return x;
+  itd_init();
+  itd_backlight(1);
+  bitmap = font88[2*c];
+  for (int b=0; b < 32; b++) {
+      v = x+(b%8);
+      w = y+(b/8);
+      on = (bitmap & (1 << (31-b)) ? 0xff : 0x00);
+      matrix_put(v,w,on);
+      itd_rect(v,w,v,w,color565(on,0,0));
+  }
+  bitmap = font88[2*c+1];
+  for (int b=0; b < 32; b++) {
+      v = x+(b%8);
+      w = y+4+(b/8);
+      on = (bitmap & (1 << (31-b)) ? 0xff : 0x00);
+      matrix_put(v,w,on);
+      itd_rect(v,w,v,w,color565(on,0,0));
+  }
 }
 
 void main(void) {
-  ordering(10,20,30,40,50,60,70);
-  serial_printhex(0, test("bar", 1023));
+  int i;
+  char buf[10];
+  int x,y;
+
+  x = 0;
+  y = 0;
+  while (1) {
+    read(0, buf, 1);
+    iprintf("%c\n", buf[0]);
+    drawchar(x,y,buf[0]);
+    x += 8;
+    if (x == 32) {
+      y += 8;  x=0;
+    }
+    if (y == 16) {
+      y = 0;
+    }
+  }
+  for (i=0; i < 128; i++) {
+    iprintf("%x\n", i);
+    drawchar(0,0, (char)i);
+    delay(0x10000);
+  }
   while (1);
 }
