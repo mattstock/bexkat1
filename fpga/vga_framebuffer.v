1fdca10fb6b74a6dfe498a23f0e93f1587660a7a
Matt Stock <stock@csgeeks.org>
2015-10-28 11:15:46 -0400

    fix typo in video output selection

--- fpga/vga_framebuffer.v
+++ fpga/vga_framebuffer.v
@@ -21,7 +21,7 @@ wire [15:0] x, y;
 assign sync_n = 1'b0;
 
 assign r = (sw ? { scandata_out[8:6], 5'b0 } : (x == 0 || x == 16'd639 ? 8'hff : { scandata_out[8:6], 5'b0 }));
-assign g = (sw ? { scandata_out[5:3], 5'b0 } : scandata_out[5:3], 5'b0 });
+assign g = (sw ? { scandata_out[5:3], 5'b0 } : { scandata_out[5:3], 5'b0 });
 assign b = (sw ? { scandata_out[2:0], 5'b0 } : (y == 0 || y == 16'd479 ? 8'hff : { scandata_out[2:0], 5'b0 }));
 
 vga_controller vga0(.active(blank_n), .vs(vs), .hs(hs), .clock(vga_clock), .reset_n(reset_n), .pixel(pixel), .x(x), .y(y));
