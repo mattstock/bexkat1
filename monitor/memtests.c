539e01f0cea7fe10f17d9ad3422114181a7b3f9e
Matt Stock <stock@csgeeks.org>
2015-11-01 22:50:05 -0500

    expanded memory testing

--- monitor/memtests.c
+++ monitor/memtests.c
@@ -3,19 +3,19 @@
 unsigned *mem = (unsigned *)0xc0000000;
 
 #define MEG (1024*1024)
-#define FULL (32*1024*1024)
+#define FULL (128*1024*1024)
 void main(void) {
   int i,f;
 
-  for (i=0; i < FULL; i++) {
+  for (i=0; i < FULL; i += 4) {
     if (i % MEG == 0)
       iprintf("%d MB write\n", i);
-    mem[i] = i;
+    mem[i>>2] = i;
   }
-  for (i=0; i < FULL; i++) {
+  for (i=0; i < FULL; i += 4) {
     if (i % MEG == 0)
       iprintf("%d MB read\n", i);
-    if (i != mem[i])
+    if (i != mem[i>>2])
       iprintf("%d failed\n");
   }
   while (1);
