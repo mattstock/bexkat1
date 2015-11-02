e75155c3a00ea5580daf2fdac0de94681ba50f23
Matt Stock <stock@csgeeks.org>
2015-11-01 22:21:37 -0500

    some sdram tests and cleanup

--- monitor/memtests.c
+++ monitor/memtests.c
@@ -1,11 +1,22 @@
-unsigned *mem = (unsigned *)0x00004000;
+#include <stdio.h>
 
+unsigned *mem = (unsigned *)0xc0000000;
+
+#define MEG (1024*1024)
+#define FULL (32*1024*1024)
 void main(void) {
   int i,f;
 
-  for (i=0; i < 32; i++)
+  for (i=0; i < FULL; i++) {
+    if (i % MEG == 0)
+      iprintf("%d MB write\n", i);
     mem[i] = i;
-  for (i=0; i < 32; i++)
-    f = mem[i];
+  }
+  for (i=0; i < FULL; i++) {
+    if (i % MEG == 0)
+      iprintf("%d MB read\n", i);
+    if (i != mem[i])
+      iprintf("%d failed\n");
+  }
   while (1);
 }
