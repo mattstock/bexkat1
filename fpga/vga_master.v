61b6167c4fcafac242a00fe00fd2fa2d38cb3e74
Matt Stock <stock@csgeeks.org>
2015-11-25 19:12:55 -0500

    pixle doubling mode for 320x240 needed for doom.  Should turn this into a mode toggle option.

--- fpga/vga_master.v
+++ fpga/vga_master.v
@@ -42,6 +42,7 @@ reg [15:0] y_sync [2:0];
 reg [31:0] rowval, rowval_next;
 reg [31:0] map_idx, map_idx_next;
 reg slave_state, slave_state_next;
+reg y_double, y_double_next;
 
 assign scanaddr = x_raw+1'b1;
 assign master_dat_o = 32'h0;
@@ -55,7 +56,7 @@ assign master_stb_o = master_cyc_o;
 assign sync_n = 1'b0;
 
 always @* begin
-  case (x_raw[1:0])
+  case (x_raw[2:1])
     2'h0: {r,g,b} = buf0_out;
     2'h1: {r,g,b} = buf1_out;
     2'h2: {r,g,b} = buf2_out;
@@ -80,12 +81,14 @@ begin
     rowval <= 32'h0;
     map_idx <= 32'h0;
     slave_state <= 1'b0;
+    y_double <= 1'b0;
   end else begin
     idx <= idx_next;
     state <= state_next;
     rowval <= rowval_next;
     map_idx <= map_idx_next;
     slave_state <= slave_state_next;
+    y_double <= y_double_next;
   end
 end
 
@@ -97,6 +100,7 @@ begin
   master_adr_o = 32'hc0000000;
   map_idx_next = map_idx;
   slave_state_next = slave_state;
+  y_double_next = y_double;
   if (slave_state == 1'b0) begin
     if (slave_cyc_i & slave_stb_i) begin
       slave_state_next = 1'b1;
@@ -123,11 +127,14 @@ begin
         state_next = STATE_BUS;
         idx_next = idx + 10'd4;
       end else begin
+        y_double_next = ~y_double;
         idx_next = 10'd0;
         if (y_sync[1] == 16'd479) begin
           rowval_next = 32'h0;
+          y_double_next = 1'b0;
         end else begin
-          rowval_next = rowval + 10'd640;
+          if (y_double)
+            rowval_next = rowval + 10'd320;
         end
         state_next = STATE_IDLE;
       end
@@ -146,13 +153,13 @@ vga_pallette map3(.clock(clk_i), .wraddress(slave_adr_i[7:0]), .wren(slave_cyc_i
   .data(slave_dat_i[23:0]), .rdaddress(map_idx[7:0]), .q(map3_out));
 // And we interleave the memory
 vgalinebuf scanline0(.wrclock(clk_i), .wraddress(idx[9:2]), .wren(state == STATE_STORE), .data(map0_out),
-  .rdclock(vga_clock), .rdaddress(scanaddr[9:2]), .q(buf0_out));
+  .rdclock(vga_clock), .rdaddress(scanaddr[10:3]), .q(buf0_out));
 vgalinebuf scanline1(.wrclock(clk_i), .wraddress(idx[9:2]), .wren(state == STATE_STORE), .data(map1_out),
-  .rdclock(vga_clock), .rdaddress(scanaddr[9:2]), .q(buf1_out));
+  .rdclock(vga_clock), .rdaddress(scanaddr[10:3]), .q(buf1_out));
 vgalinebuf scanline2(.wrclock(clk_i), .wraddress(idx[9:2]), .wren(state == STATE_STORE), .data(map2_out),
-  .rdclock(vga_clock), .rdaddress(scanaddr[9:2]), .q(buf2_out));
+  .rdclock(vga_clock), .rdaddress(scanaddr[10:3]), .q(buf2_out));
 vgalinebuf scanline3(.wrclock(clk_i), .wraddress(idx[9:2]), .wren(state == STATE_STORE), .data(map3_out),
-  .rdclock(vga_clock), .rdaddress(scanaddr[9:2]), .q(buf3_out));
+  .rdclock(vga_clock), .rdaddress(scanaddr[10:3]), .q(buf3_out));
 
 vga_controller vga0(.active(blank_n), .vs(vs), .hs(hs), .clock(vga_clock), .reset_n(~rst_i), .x(x_raw), .y(y_raw));
 vgapll vgapll0(.inclk0(clk_i), .c0(vga_clock));
