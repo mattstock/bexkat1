7f6df445f8db7e0981c2c337af0ec5c453d366f0
Matt Stock <stock@csgeeks.org>
2016-01-06 14:50:11 -0500

    fib test back to normal

--- monitor/fib.c
+++ monitor/fib.c
@@ -3,9 +3,8 @@
 #include "matrix.h"
 #include "vectors.h"
 
-unsigned int *timers = (unsigned int *)0x30008000;
-
 unsigned fib(unsigned short x);
+
 
 unsigned fib(unsigned short x) {
   if (x == 0)
@@ -15,35 +14,9 @@ unsigned fib(unsigned short x) {
   return fib(x-1) + fib(x-2);
 }
 
-void timer0(void) {
-  matrix_put(0,0,0xff);
-  timers[1] &= 0xfffffffe; // clear the fire
-  timers[4] += 500000; // reset timer0 interval
-}
-
-void timer1(void) {
-  matrix_put(0,1,0xff00);
-  timers[1] &= 0xfffffffd; // clear the fire
-  timers[5] += 50000000; // reset timer1 interval
-}
-
-void timer0() __attribute__ ((interrupt_handler));
-void timer1() __attribute__ ((interrupt_handler));
-  
 void main(void) {
   unsigned v;
-  unsigned int count = 50000000; // 1Hz
 
-  timers[4] = timers[12] + 500000; // 100Hz
-  timers[5] = timers[12] + 50000000; // 1Hz
-  _vectors_start[2] = &timer0;
-  _vectors_start[3] = &timer1;
-  timers[0] |= 0x33; // enable timer and timer interrupt
-  asm("sti");
-  while (1) {
-    matrix_put(0,3, 0xff00);
-  };
-  
   matrix_bitprint(1, 0xffffffff); 
   matrix_bitprint(2, 0xa0244fd1); 
   matrix_bitprint(3, 0xaaaa5555); 
