98a0a1d87eb124b39ab6c6623752f9e8cdaa11b5
Matt Stock <stock@csgeeks.org>
2016-02-04 22:50:16 -0500

    small cleanups

--- fpga/flash_controller.v
+++ fpga/flash_controller.v
@@ -20,8 +20,5 @@ module flash_controller(
  // This controller will do the work related to page mode reads, etc.
  // the adapt32to16 will adjust for the bus widths by adding an additional bus cycle.
  
-adapt32to16 width0(.clock(clock), .reset_n(reset_n), .read(read), .write(write), .data_in(data_in), .data_out(data_out),
-  .databus_in(databus_in), .databus_out(databus_out), .ready(ready), .wp_n(wp_n), .ce_n(ce_n), .oe_n(oe_n), .we_n(we_n),
-  .address_in(address_in), .address_out(address_out), .wait_out(wait_out), .be_in(be_in));
   
 endmodule
