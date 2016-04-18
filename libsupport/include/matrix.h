ddefe43b21172f6df4f802f1403cf8795b2c3d2f
Matt Stock <stock@csgeeks.org>
2016-04-17 22:55:52 -0400

    add line and circle drawing

--- libsupport/include/matrix.h
+++ libsupport/include/matrix.h
@@ -4,6 +4,9 @@
 extern unsigned *matrix;
 
 extern void matrix_init(void);
+extern void matrix_line(unsigned x0, unsigned y0, unsigned x1, unsigned y1, unsigned val);
+extern void matrix_circle(unsigned x0, unsigned y0, unsigned r, unsigned val);
+
 extern void matrix_put(unsigned x, unsigned y, unsigned val);
 extern void matrix_bitprint(unsigned row, unsigned val);
 
--- libsupport/matrix.c
+++ libsupport/matrix.c
@@ -1,4 +1,5 @@
 #include "matrix.h"
+#include <stdlib.h>
 
 unsigned *matrix = (unsigned *)0x20000000;
 
@@ -6,6 +7,47 @@ void matrix_put(unsigned x, unsigned y, unsigned val) {
   if (y > 15 || x > 31)
     return;
   matrix[y*32+x] = val;
+}
+
+void matrix_line(unsigned x0, unsigned y0, unsigned x1, unsigned y1, unsigned val) {
+  int dx = abs(x1 - x0);
+  int sx = (x0<x1 ? 1 : -1);
+  int dy = abs(y1 - y0);
+  int sy = (y0<y1 ? 1 : -1);
+  int err = (dx>dy ? dx : -dy)/2;
+  int e2;
+
+  while (1) {
+    matrix_put(x0,y0,val);
+    if (x0 == x1 && y0 == y1) break;
+    e2 = err;
+    if (e2 > -dx) { err -= dy; x0 += sx; }
+    if (e2 < dy) { err += dx; y0 += sy; }
+  }
+}
+
+void matrix_circle(unsigned x0, unsigned y0, unsigned r, unsigned val) {
+  int d = 1 - r;
+  int x = r;
+  int y = 0;
+
+  while (y <= x) {
+    matrix_put(x+x0, y+y0, val);
+    matrix_put(y+x0, x+y0, val);
+    matrix_put(x0-x, y+y0, val);
+    matrix_put(x0-y, x+y0, val);
+    matrix_put(x0-x, y0-y, val);
+    matrix_put(x0-y, y0-x, val);
+    matrix_put(x+x0, y0-y, val);
+    matrix_put(y+x0, y0-x, val);
+    y++;
+    if (d <= 0) {
+      d += 2*y+1;
+    } else {
+      x--;
+      d += 2*(y-x)+1;
+    }
+  }
 }
 
 void matrix_init() {
