2f0ae748795f53189241637b9e1e7b15bcb162b3
Matt Stock <mstock@bexkat.com>
2016-12-17 15:35:36 -0500

    refactor vga in preparation for a text modem driver

--- fpga/vga/vga_master.v
+++ fpga/vga/vga_master.v
@@ -33,31 +33,27 @@ module vga_master(
 // 0xc00 - video memory base address
 // 0xc01 - video mode, palette select
 
-localparam [1:0] STATE_IDLE = 2'h0, STATE_BUS = 2'h1, STATE_PALETTE = 2'h2, STATE_STORE = 2'h3;
 localparam [2:0] SSTATE_IDLE = 3'h0, SSTATE_PALETTE = 3'h1, SSTATE_PALETTE2 = 3'h2, SSTATE_FONT = 3'h3, SSTATE_DONE = 3'h4;
 
-wire [23:0] buf0_out, buf1_out, buf2_out, buf3_out;
-wire [23:0] map0_out, map1_out, map2_out, map3_out;
+logic [2:0] sstate, sstate_next;
+logic [31:0] setupreg, setupreg_next, vgabase, vgabase_next;
+logic [31:0] slave_dat_o_next;
+
+wire [7:0] gd_r, gd_g, gd_b;
+wire gd_cyc_o;
+wire [31:0] gd_adr_o;
 wire [15:0] x_raw, y_raw;
-wire [15:0] scanaddr;
 
-reg [1:0] state, state_next;
-reg [9:0] idx, idx_next;
-reg [15:0] x_sync [2:0];
-reg [15:0] y_sync [2:0];
-reg [31:0] rowval, rowval_next;
-reg [31:0] setupreg, setupreg_next, vgabase, vgabase_next;
-reg [31:0] map_idx, map_idx_next, slave_dat_o_next;
-reg [2:0] sstate, sstate_next;
-reg y_double, y_double_next;
-
-assign scanaddr = x_raw+1'b1;
 assign master_dat_o = 32'h0;
 assign slave_ack_o = (sstate == SSTATE_DONE);
 assign master_we_o = 1'b0;
 assign master_sel_o = 4'hf;
-assign master_cyc_o = (state == STATE_BUS);
+assign master_cyc_o = gd_cyc_o;
 assign master_stb_o = master_cyc_o;
+assign master_adr_o = vgabase + gd_adr_o;
+assign r = gd_r;
+assign g = gd_g;
+assign b = gd_b;
 
 assign sync_n = 1'b0;
 
@@ -77,7 +73,7 @@ begin
   end
 end
 
-always @(*)
+always_comb
 begin
   sstate_next = sstate;
   setupreg_next = setupreg;
@@ -135,106 +131,14 @@ begin
   endcase
 end
 
-wire pixeldouble = (setupreg[1:0] == 2'b01);
+graphicsdrv graphicsdriver0(.clk_i(clk_i), .rst_i(rst_i), .pixeldouble(setupreg[1:0] == 2'b01), .x(x_raw), .y(y_raw),
+  .r(gd_r), .g(gd_g), .b(gd_b), .cyc_o(gd_cyc_o), .palette_write((sstate == SSTATE_PALETTE) & slave_we_i),
+  .ack_i(master_ack_i), .palette_adr_i(slave_adr_i[7:0]), .palette_dat_i(slave_dat_i[23:0]),
+  .adr_o(gd_adr_o), .dat_i(master_dat_i), .vga_clock(vga_clock));
 
-always_comb
-begin
-  case ((pixeldouble ? x_raw[2:1] : x_raw[1:0]))
-    2'h0: {r,g,b} = buf0_out;
-    2'h1: {r,g,b} = buf1_out;
-    2'h2: {r,g,b} = buf2_out;
-    2'h3: {r,g,b} = buf3_out;
-  endcase
-end
-
-always @(posedge clk_i) begin
-  x_sync[2] <= x_sync[1];
-  x_sync[1] <= x_sync[0];
-  x_sync[0] <= x_raw;
-  y_sync[2] <= y_sync[1];
-  y_sync[1] <= y_sync[0];
-  y_sync[0] <= y_raw;
-end
-
-// Master state machine
-always @(posedge clk_i or posedge rst_i)
-begin
-  if (rst_i) begin
-    state <= STATE_IDLE;
-    idx <= 10'h0;
-    rowval <= 32'h0;
-    map_idx <= 32'h0;
-    y_double <= 1'b0;
-  end else begin
-    idx <= idx_next;
-    state <= state_next;
-    rowval <= rowval_next;
-    map_idx <= map_idx_next;
-    y_double <= y_double_next;
-  end
-end
-
-always @(*)
-begin
-  state_next = state;
-  idx_next = idx;
-  rowval_next = rowval;
-  master_adr_o = vgabase;
-  map_idx_next = map_idx;
-  y_double_next = y_double;
-  case (state)
-    STATE_IDLE: begin
-      if (y_sync[2] != y_sync[1]) begin
-        state_next = STATE_BUS;
-      end
-    end
-    STATE_BUS: begin
-      master_adr_o = vgabase + rowval + idx;
-      if (master_ack_i) begin
-        state_next = STATE_PALETTE;
-        map_idx_next = master_dat_i;
-      end
-    end
-    STATE_PALETTE: state_next = STATE_STORE;
-    STATE_STORE: begin
-      if (idx < 10'd640) begin
-        state_next = STATE_BUS;
-        idx_next = idx + 10'd4;
-      end else begin
-        y_double_next = ~y_double;
-        idx_next = 10'd0;
-        if (y_sync[1] == 16'd479) begin
-          rowval_next = 32'h0;
-          y_double_next = 1'b0;
-        end else begin
-          rowval_next = (pixeldouble ? (y_double ? rowval + 10'd320 : rowval) : rowval + 10'd640);
-        end
-        state_next = STATE_IDLE;
-      end
-    end
-  endcase
-end
-
-// We duplicate the palette map to allow for parallel handling of the reads later
-vga_pallette map0(.clock(clk_i), .wraddress(slave_adr_i[7:0]), .wren((sstate == SSTATE_PALETTE) & slave_we_i),
-  .data(slave_dat_i[23:0]), .rdaddress(map_idx[31:24]), .q(map0_out));
-vga_pallette map1(.clock(clk_i), .wraddress(slave_adr_i[7:0]), .wren((sstate == SSTATE_PALETTE) & slave_we_i),
-  .data(slave_dat_i[23:0]), .rdaddress(map_idx[23:16]), .q(map1_out));
-vga_pallette map2(.clock(clk_i), .wraddress(slave_adr_i[7:0]), .wren((sstate == SSTATE_PALETTE) & slave_we_i),
-  .data(slave_dat_i[23:0]), .rdaddress(map_idx[15:8]), .q(map2_out));
-vga_pallette map3(.clock(clk_i), .wraddress(slave_adr_i[7:0]), .wren((sstate == SSTATE_PALETTE) & slave_we_i),
-  .data(slave_dat_i[23:0]), .rdaddress(map_idx[7:0]), .q(map3_out));
-// And we interleave the memory
-vgalinebuf scanline0(.wrclock(clk_i), .wraddress(idx[9:2]), .wren(state == STATE_STORE), .data(map0_out),
-  .rdclock(vga_clock), .rdaddress((pixeldouble ? scanaddr[10:3] : scanaddr[9:2])), .q(buf0_out));
-vgalinebuf scanline1(.wrclock(clk_i), .wraddress(idx[9:2]), .wren(state == STATE_STORE), .data(map1_out),
-  .rdclock(vga_clock), .rdaddress((pixeldouble ? scanaddr[10:3] : scanaddr[9:2])), .q(buf1_out));
-vgalinebuf scanline2(.wrclock(clk_i), .wraddress(idx[9:2]), .wren(state == STATE_STORE), .data(map2_out),
-  .rdclock(vga_clock), .rdaddress((pixeldouble ? scanaddr[10:3] : scanaddr[9:2])), .q(buf2_out));
-vgalinebuf scanline3(.wrclock(clk_i), .wraddress(idx[9:2]), .wren(state == STATE_STORE), .data(map3_out),
-  .rdclock(vga_clock), .rdaddress((pixeldouble ? scanaddr[10:3] : scanaddr[9:2])), .q(buf3_out));
+// Font memory
+//fontmem font0(.clock(clk_i), .address(), .q(font));
 
 vga_controller vga0(.active(blank_n), .vs(vs), .hs(hs), .clock(vga_clock), .reset_n(~rst_i), .x(x_raw), .y(y_raw));
-
 
 endmodule
