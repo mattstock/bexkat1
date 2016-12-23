bbef87431d44edaa6ccc6942753b88882672294a
Matt Stock <stock@bexkat.com>
2016-12-22 21:34:18 -0500

    added color text modes

--- fpga/vga/textdrv.v
+++ fpga/vga/textdrv.v
@@ -16,10 +16,11 @@ module textdrv(
 
 wire [95:0] fontval;
 wire [31:0] char;
-wire [95:0] font0_out, font1_out, font2_out, font3_out;
+wire [95:0] font0_out, font1_out;
 wire [31:0] buf_out;
 wire [15:0] scanaddr;
 wire [15:0] textrow, textcol;
+wire [7:0] color0, color1;
 wire oncursor;
 
 logic [1:0] state, state_next;
@@ -42,23 +43,25 @@ assign oncursor = ({textrow,textcol} == cursorpos) && ((blink[25] & cursormode[3
 always_comb
 begin
   case (y[2:0])
-//    'd11: char = { font0_out[7:0], font1_out[7:0], font2_out[7:0], font3_out[7:0] };
-//    'd10: char = { font0_out[15:8], font1_out[15:8], font2_out[15:8], font3_out[15:8] };
-//    'd9: char = { font0_out[23:16], font1_out[23:16], font2_out[23:16], font3_out[23:16] };
-//    'd8: char = { font0_out[31:24], font1_out[31:24], font2_out[31:24], font3_out[31:24] };
-    'h7: char = { font0_out[39:32], font1_out[39:32], font2_out[39:32], font3_out[39:32] };
-    'h6: char = { font0_out[47:40], font1_out[47:40], font2_out[47:40], font3_out[47:40] };
-    'h5: char = { font0_out[55:48], font1_out[55:48], font2_out[55:48], font3_out[55:48] };
-    'h4: char = { font0_out[63:56], font1_out[63:56], font2_out[63:56], font3_out[63:56] };
-    'h3: char = { font0_out[71:64], font1_out[71:64], font2_out[71:64], font3_out[71:64] };
-    'h2: char = { font0_out[79:72], font1_out[79:72], font2_out[79:72], font3_out[79:72] };
-    'h1: char = { font0_out[87:80], font1_out[87:80], font2_out[87:80], font3_out[87:80] };
-    'h0: char = { font0_out[95:88], font1_out[95:88], font2_out[95:88], font3_out[95:88] };
-	 default: char = 32'h0;
+    'h7: char = { color0, color1, font0_out[39:32], font1_out[39:32] };
+    'h6: char = { color0, color1, font0_out[47:40], font1_out[47:40] };
+    'h5: char = { color0, color1, font0_out[55:48], font1_out[55:48] };
+    'h4: char = { color0, color1, font0_out[63:56], font1_out[63:56] };
+    'h3: char = { color0, color1, font0_out[71:64], font1_out[71:64] };
+    'h2: char = { color0, color1, font0_out[79:72], font1_out[79:72] };
+    'h1: char = { color0, color1, font0_out[87:80], font1_out[87:80] };
+    'h0: char = { color0, color1, font0_out[95:88], font1_out[95:88] };
+	 default: char = 16'h0;
   endcase
 end  
 
-assign {r,g,b} = (buf_out[5'd31-x[4:0]] || oncursor ? 24'hffffff : 24'h000000);
+wire [7:0] red, green, blue;
+
+assign red =   (x[3] ? { buf_out[23:22], 6'h0 } : { buf_out[31:30], 6'h0 });
+assign green = (x[3] ? { buf_out[21:19], 5'h0 } : { buf_out[29:27], 5'h0 });
+assign blue =  (x[3] ? { buf_out[18:16], 5'h0 } : { buf_out[26:24], 5'h0 });
+ 
+assign {r,g,b} = (buf_out[4'hf-x[3:0]] || oncursor ? { red, green, blue } : 24'h000000);
 
 always @(posedge clk_i) begin
   x_sync[2] <= x_sync[1];
@@ -109,7 +112,7 @@ begin
     end
 	 STATE_FONT: state_next = STATE_STORE;
     STATE_STORE: begin
-      if (idx < 10'd80) begin
+      if (idx < 11'd160) begin
         state_next = STATE_BUS;
         idx_next = idx + 10'd4;
       end else begin
@@ -125,13 +128,12 @@ begin
   endcase
 end
 
-// Font memory x4 so we can decode a word at a time.
-fontmem font0(.clock(clk_i), .address(font_idx[30:24]), .q(font0_out));
-fontmem font1(.clock(clk_i), .address(font_idx[22:16]), .q(font1_out));
-fontmem font2(.clock(clk_i), .address(font_idx[14:8]), .q(font2_out));
-fontmem font3(.clock(clk_i), .address(font_idx[6:0]), .q(font3_out));
+assign color0 = font_idx[31:24];
+fontmem font0(.clock(clk_i), .address(font_idx[22:16]), .q(font0_out));
+assign color1 = font_idx[15:8];
+fontmem font1(.clock(clk_i), .address(font_idx[6:0]), .q(font1_out));
 
-textlinebuf linebuf0(.wrclock(clk_i), .wraddress(idx[7:2]), .wren(state == STATE_STORE), .data(char),
-  .rdclock(vga_clock), .rdaddress(scanaddr[11:5]), .q(buf_out));
+textlinebuf linebuf0(.wrclock(clk_i), .wraddress(idx[9:2]), .wren(state == STATE_STORE), .data(char),
+  .rdclock(vga_clock), .rdaddress(scanaddr[11:4]), .q(buf_out));
 
 endmodule
