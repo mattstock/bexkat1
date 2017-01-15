c0d2f680f52bedb28aa98c9cd7197a9ee86ff664
Matt Stock <stock@csgeeks.org>
2017-01-15 11:57:11 -0500

    improved timer functions

--- libsupport/include/timers.h
+++ libsupport/include/timers.h
@@ -1,6 +1,21 @@
 #ifndef TIMERS_H
 #define TIMERS_H
 
-unsigned int *timers;
+#include <machine.h>
+
+/* register defines */
+#define TIMER_CONTROL 0
+#define TIMER_STATUS 1
+#define TIMER_COMPARE0 4
+#define TIMER_COMPARE1 5
+#define TIMER_COMPARE2 6
+#define TIMER_COMPARE3 7
+#define TIMER_COUNTER0 8
+#define TIMER_COUNTER1 9
+#define TIMER_COUNTER2 10
+#define TIMER_COUNTER3 11
+#define TIMER_FREE 12
+
+extern volatile unsigned int * const timers;
 
 #endif
--- libsupport/timers.c
+++ libsupport/timers.c
@@ -1,3 +1,7 @@
 #include "timers.h"
 
-unsigned int *timers = (unsigned int *)0x30008000;
+volatile unsigned int * const timers = (unsigned int *)TIMER_BASE;
+
+unsigned int ticks() {
+  return timers[TIMER_FREE];
+}
