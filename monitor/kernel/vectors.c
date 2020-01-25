a331b326e0536657a3b9d57b1b0a9d46d41ee3a8
Matt Stock <stock@bexkat.com>
2020-01-25 17:01:23 -0500

    improved syntax

--- monitor/kernel/vectors.c
+++ monitor/kernel/vectors.c
@@ -6,23 +6,23 @@ static void dummy(void) {
   asm("halt");
 }
 
-vector_t _vectors_start[16] = {
-  JMPD, (void *)_start, /* reset */
-  JMPD, dummy, /* mmu fault */
-  JMPD, dummy, /* timer0 */
-  JMPD, dummy, /* timer1 */
-  JMPD, dummy, /* timer2 */
-  JMPD, dummy, /* timer3 */
-  JMPD, dummy, /* uart0 rx */
-  JMPD, dummy, /* uart0 tx */
-  JMPD, dummy, /* illegal instruction */
-  JMPD, dummy, /* cpu1 */
-  JMPD, dummy, /* cpu2 */
-  JMPD, dummy, /* cpu3 */
-  JMPD, dummy, /* trap 0 */
-  JMPD, dummy, /* trap 1 */
-  JMPD, dummy, /* trap 2 */
-  JMPD, dummy /* trap 3 */
+vector_t _vectors_start[] __attribute__ ((aligned (4))) = {
+			     {JMPD, (void *)_start}, /* reset */
+			     {JMPD, dummy}, /* mmu fault */
+			     {JMPD, dummy}, /* timer0 */
+			     {JMPD, dummy}, /* timer1 */
+			     {JMPD, dummy}, /* timer2 */
+			     {JMPD, dummy}, /* timer3 */
+			     {JMPD, dummy}, /* uart0 rx */
+			     {JMPD, dummy}, /* uart0 tx */
+			     {JMPD, dummy}, /* illegal instruction */
+			     {JMPD, dummy}, /* cpu1 */
+			     {JMPD, dummy}, /* cpu2 */
+			     {JMPD, dummy}, /* cpu3 */
+			     {JMPD, dummy}, /* trap 0 */
+			     {JMPD, dummy}, /* trap 1 */
+			     {JMPD, dummy}, /* trap 2 */
+			     {JMPD, dummy} /* trap 3 */
 };
 
 void set_interrupt_handler(interrupt_slot s, isr f) {
